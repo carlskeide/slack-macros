43ea7a55c59b95f29815535b491446188513ba74
Carl Skeide <carl@skeide.se>
2020-11-18 01:31:00 +0000

    Make path prefix configurable

--- src/config.py
+++ src/config.py
@@ -12,9 +12,9 @@ CACHE_REDIS_URL = sec.load("redis_url", fallback="redis://localhost")
 CACHE_DEFAULT_TIMEOUT = 600
 
 # Flack
-FLACK_URL_PREFIX = "/"
 FLACK_DEFAULT_NAME = "TzBot"
 FLACK_TOKEN = sec.load("slack_token")
+FLACK_URL_PREFIX = sec.load("flack_prefix", fallback="/")
 
 # App
 RESOURCES_DIR = "./resources"
